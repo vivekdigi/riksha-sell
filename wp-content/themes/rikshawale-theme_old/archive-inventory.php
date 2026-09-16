b8e712a1b54e2278933c30f4d6296176884de765
Vivek Thakur <digiflexvivek@gmail.com>
2026-09-16 15:11:19 +0530

    chore: remove file

--- wp-content/themes/rikshawale-theme_old/archive-inventory.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Archive Inventory Template
- * Automatically uses the Riksha Inventory Filter Template for /inventory/ archive endpoint.
- */
-
-require get_template_directory() . '/template-inventory-filter.php';
--- wp-content/themes/rikshawale-theme_old/taxonomy-riksha_location.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Taxonomy Archive Template for Riksha Locations / Places
- */
-
-require get_template_directory() . '/template-inventory-filter.php';
